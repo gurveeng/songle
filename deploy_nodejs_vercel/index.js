22969be394a6a4e169f309b8ecb094e410017bbc
gurveeng <gurveenngill@gmail.com>
2024-04-27 15:59:54 -0700

    updated requirements

--- deploy_nodejs_vercel/index.js
+++ deploy_nodejs_vercel/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const fetch = require('node-fetch');
 const querystring = require('querystring');
 const crypto = require('crypto');
 const session = require('express-session');
